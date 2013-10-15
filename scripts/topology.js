342d28d171a47e5dd3d739c23b50d69c39d98e3b
andrejlucansky <andrejlucansky@gmail.com>
2013-10-15 19:36:54 +0200

    1. breaker nodes removal on parallel lines

--- scripts/topology.js
+++ scripts/topology.js
@@ -17,29 +17,120 @@ d3.json("../data/topology.json", function(error, json){
         .start();
 
     var nodeDrag = force.drag()
-        .on("dragstart", function(d){
+        .on("dragstart", function (d) {
             d.fixed = true;
             d3.select(this).classed("fixed", true);
+        })
+        .on("dragend", function (d, i) {
+            //get type of dragged node
+            var nodeType = d.type;
+            //if it is invisible node, get links where the node is parent(2)
+            if (nodeType == "invisible") {
+                var nodeLinks = new Array();
+                links.forEach(function (l) {
+                    if (l.source == d)
+                        nodeLinks.push(l);
+                });
+
+                //if there are 2 links(must be) check if they are parallel
+                if (nodeLinks.length == 2) {
+                    var segment1 = [d, nodeLinks[0].target];
+                    var segment2 = [d, nodeLinks[1].target];
+                    var areParallel = computeParallelism(segment1, segment2);
+                                    console.log("computing");
+                    if(areParallel){
+                        //add links from source to target
+                        links.push({
+                            "source" : nodeLinks[0].target,
+                            "target" : nodeLinks[1].target,
+                            "type" : "upload"
+                        });
+
+                        links.push({
+                            "source" : nodeLinks[1].target,
+                            "target" : nodeLinks[0].target,
+                            "type" : "upload"
+                        });
+
+                        //remove links
+                        var linksForDelete = [];
+                        links.forEach(function(l){
+                            if(l.source == d || l.target == d)  {
+                                linksForDelete.push(l);
+                            }
+                        });
+
+                        for(var i = 0; i < linksForDelete.length; i++){
+                            links.splice(links.indexOf(linksForDelete[i]),1);
+                        }
+                        link.data(links).exit().remove();
+
+                        //remove node
+                        nodes.splice(nodes.indexOf(d), 1);
+                        node.data(nodes).exit().remove();
+                    }
+                }
+            }
         });
 
-    var lineDrag = force.drag()
-        .on("dragstart", function(d){
-            //breaking lines here
-        })
-        .on("dragend", function(d){
-           //stop move
-        });
+    function restart() {
+        link = link.data(links);
+
+        link.enter().insert("line", ".node")
+            .attr("class", "link");
+
+        node = node.data(nodes);
+
+        force.start();
+
+        node.enter()
+            .insert("circle", ".cursor")
+            .attr("class", "node")
+            .attr("r", 5)
+            .attr("id", function(d){return d.index;})
+            .call(nodeDrag);
+    }
 
     //straight lines
     var link = svg.selectAll(".link").data(links).enter().append("line")
         .attr("class", "link")
         .attr("sourceid", function(d){ return d.source.index})
-        .attr("targetid", function(d){ return d.target.index});
+        .attr("targetid", function(d){ return d.target.index})
+        .on("mousedown", function(d){
+            //create drag node
+            var coordinates = [0, 0];
+            coordinates = d3.mouse(this);
+            var x = coordinates[0];
+            var y = coordinates[1];
+
+            var n = {
+                "type" : "invisible",
+                "image" : undefined,
+                "x" : x,
+                "y" : y,
+                "fixed" : true
+            }
+            nodes.push(n);
+            links.push({"source" : d.source, "target" : n, "type" : "upload"});
+            links.push({"source" : n, "target" : d.target, "type" : "upload"});
+            links.push({"source" : d.target, "target" : n, "type" : "upload"});
+            links.push({"source" : n, "target" : d.source, "type" : "upload"});
+
+            links.forEach(function(l){
+                if(l.source == d.target && l.target == d.source)
+                    links.splice(links.indexOf(l),1);
+            });
+            links.splice(links.indexOf(d),1);
+            link.data(links).exit().remove();   //nefunguje, treba zacat indexovat elementy klucom
+
+            restart();
+            //simulate(document.getElementById(n.index), "mousedown", {pointerX: d3.mouse(this)[0], pointerY: d3.mouse(this)[1]});
+        });
 
 //Nodes represented by images
     var node = svg.selectAll(".node").data(nodes).enter().append("g")
         .attr("class", "node")
-        .attr("index", function(d){return d.index})
+        .attr("id", function(d){return d.index})
         .call(nodeDrag);
 
     node.append("image")
@@ -54,13 +145,6 @@ d3.json("../data/topology.json", function(error, json){
         .attr("dy", ".35em")
         .attr("class", "label")
         .text(function(d) { return d.type });
-
-    function end(){
-        for (var i = 0; i < nodes.length; i++){
-            nodes[i].fixed = true;
-        }
-        d3.selectAll(".node").classed("fixed", true);
-    }
 
     function tick() {
         node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
@@ -104,8 +188,45 @@ d3.json("../data/topology.json", function(error, json){
                      return  computeCoordinates2(d.target.x, d.target.y, d.source.x, d.source.y)[1];
                  }
              })
-             .style("stroke", function(d) {if(d.source.index > d.target.index) return "green"; else return "red";});
+             .style("stroke", function(d) {
+                    if(d.source.index > d.target.index)
+                        return "blue";
+                    else
+                        return "red";
+             });
 
+    }
+
+    function end(){
+        for (var i = 0; i < nodes.length; i++){
+            nodes[i].fixed = true;
+        }
+        d3.selectAll(".node").classed("fixed", true);
+    }
+
+    function computeParallelism(segment1, segment2){
+        //compute direction vector of the first segment
+        var u1 = segment1[1].x - segment1[0].x;
+        var u2 = segment1[1].y - segment1[0].y;
+
+        //compute direction vector of the second segment
+        var v1 = segment2[1].x - segment2[0].x;
+        var v2 = segment2[1].y - segment2[0].y;
+
+        console.log(roundNumber(u1/v1, 1));
+        console.log(roundNumber(u2/v2, 1));
+
+        //compare ratio of first and second part of direction vectors rounded to 1 decimal number
+        if(roundNumber(u1/v1, 1) == roundNumber(u2/v2, 1))
+            return true;
+        else
+            return false;
+    }
+
+    function roundNumber(number, digits) {
+        var multiple = Math.pow(10, digits);
+        var rndedNum = Math.round(number * multiple) / multiple;
+        return rndedNum;
     }
 
     function computeCoordinates(source_x, source_y, target_x, target_y){
@@ -167,5 +288,66 @@ d3.json("../data/topology.json", function(error, json){
         else
             var new_c2 = -(Math.sqrt(Math.pow((c2-a2),2) * ratio)) + a2;
         return [new_c1,new_c2];
+    }
+
+    function simulate(element, eventName)
+    {
+        var options = extend(defaultOptions, arguments[2] || {});
+        var oEvent, eventType = null;
+
+        for (var name in eventMatchers)
+        {
+            if (eventMatchers[name].test(eventName)) { eventType = name; break; }
+        }
+
+        if (!eventType)
+            throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+        if (document.createEvent)
+        {
+            oEvent = document.createEvent(eventType);
+            if (eventType == 'HTMLEvents')
+            {
+                oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+            }
+            else
+            {
+                oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+                    options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+                    options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+            }
+            element.dispatchEvent(oEvent);
+        }
+        else
+        {
+            options.clientX = options.pointerX;
+            options.clientY = options.pointerY;
+            var evt = document.createEventObject();
+            oEvent = extend(evt, options);
+            element.fireEvent('on' + eventName, oEvent);
+        }
+        return element;
+    }
+
+    function extend(destination, source) {
+        for (var property in source)
+            destination[property] = source[property];
+        return destination;
+    }
+
+    var eventMatchers = {
+        'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+        'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+    }
+    var defaultOptions = {
+        pointerX: 0,
+        pointerY: 0,
+        button: 0,
+        ctrlKey: false,
+        altKey: false,
+        shiftKey: false,
+        metaKey: false,
+        bubbles: true,
+        cancelable: true
     }
 });
